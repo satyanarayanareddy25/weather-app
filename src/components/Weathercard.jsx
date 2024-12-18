e028a5a4044aecbbc706825bfec11d0f193bbbec
satya <satyanarayana00789@gmail.com>
2024-12-18 13:36:03 +0530

    updated styles

--- src/components/Weathercard.jsx
+++ src/components/Weathercard.jsx
@@ -56,30 +56,29 @@ const WeatherCard = ({ cityKey }) => {
   return (
     <div className="weather-container">
       <h1 className="city-name">{location.name} , {location.country}</h1>
+      {/* <h1 className="date">{forecast.forecastday[0].date}</h1> */}
   <ul className="weather-row">
  
     <li className="current-card card">
       <h2>Current Weather</h2>
-      <p>Local Time: {location.localtime}</p>
-        <p>Temperature: {current.temp_c}°C</p>
-        <p>Condition: {current.condition.text}</p>
-        <p>Wind Speed: {current.wind_kph} km/h</p>
-        <p>UV Index: {current.uv}</p>
+        <p><b>Temperature</b>: {current.temp_c}°C</p>
+        <p><b>Condition</b>: {current.condition.text}</p>
+        <p><b>Wind Speed</b>: {current.wind_kph} km/h</p>
+        <p><b>UV Index</b>: {current.uv}</p>
     </li>
 
     <li className="weather-card card">
-      <h2>Today report {forecast.forecastday[0].date}</h2>
-      
-      <p>Maximium Temperature: {forecast.forecastday[0].day.maxtemp_c}°C</p>
-      <p>Minimum Temperature: {forecast.forecastday[0].day.mintemp_c}°C</p>
-      <p>Condition: {forecast.forecastday[0].day.condition.text}</p>
-      <p>Precipitation Chance: {forecast.forecastday[0].day.daily_chance_of_rain}%</p>
+      <h2>Today report </h2>
+      <p><b>Max Temperature</b>: {forecast.forecastday[0].day.maxtemp_c}°C</p>
+      <p><b>Min Temperature</b>: {forecast.forecastday[0].day.mintemp_c}°C</p>
+      <p><b>Condition</b>: {forecast.forecastday[0].day.condition.text}</p>
+      <p><b>Precipitation </b>: {forecast.forecastday[0].day.daily_chance_of_rain}%</p>
     </li>
 
     <li className="astro-card card">
-      <p>Sunrise: {forecast.forecastday[0].astro.sunrise}</p>
-      <p>Sunset : {forecast.forecastday[0].astro.sunset}</p>
-     
+      <p><b>Sunrise</b>: {forecast.forecastday[0].astro.sunrise}</p>
+      <p><b>Sunset </b>: {forecast.forecastday[0].astro.sunset}</p>
+      <img src={current.condition.icon} alt="weather"></img>
     </li>
   </ul>
 </div>
