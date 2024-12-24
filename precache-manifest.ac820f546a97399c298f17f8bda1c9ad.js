16cdbc6881cf4c77426fe268e5aa5b5e4e60baad
satya <satyanarayana00789@gmail.com>
2024-12-24 18:39:31 +0530

    Updates

---
+++ precache-manifest.ac820f546a97399c298f17f8bda1c9ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eac0f6fe9e9426482e044dddb1878fd",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "dabd04e91f8529d3667f",
+    "url": "/weather-app/static/css/main.89110ec0.chunk.css"
+  },
+  {
+    "revision": "d9006015b1f3892c276a",
+    "url": "/weather-app/static/js/2.61b6401c.chunk.js"
+  },
+  {
+    "revision": "d4dfc75ab6596c05c81c",
+    "url": "/weather-app/static/js/3.731075be.chunk.js"
+  },
+  {
+    "revision": "dabd04e91f8529d3667f",
+    "url": "/weather-app/static/js/main.decb8175.chunk.js"
+  },
+  {
+    "revision": "a7977ee9aecbb4238277",
+    "url": "/weather-app/static/js/runtime~main.abbefff8.js"
+  },
+  {
+    "revision": "5fd90fca2a2d6b4a0203a23819f248e6",
+    "url": "/weather-app/static/media/background.5fd90fca.webp"
+  }
+]);
